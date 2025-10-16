c3fe228e5de7ac15d2394e68834b8b2e70b1c362
Jack Hogan <jackhogan11@gmail.com>
2025-10-16 09:14:18 -0400

    Node and docker

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 import svelte from "@astrojs/svelte";
 import sharpService from "astro/assets/services/sharp";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   image: {
@@ -13,9 +15,14 @@ export default defineConfig({
       entrypoint: "@lib/sharpdither.ts",
     },
   },
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [svelte()],
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
